a338a6f502af70e837290a92c4f8995ab3d81284
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-01-24 22:07:39 +0100

    4ème commit

--- scripts/combocategory.js
+++ scripts/combocategory.js
@@ -10,48 +10,35 @@ async function getCategories(url) {
     }
 }
 
-async function renderCategories() {
-  const total_pages = 5;
-  var comboList = document.createElement('datalist');
-  comboList.id = "category_list";
-  for (let currentPage = 1; currentPage <= total_pages; currentPage++) { 
-    url = `${GENRES_API_URL}?page=${currentPage}`;
-    let data_cat = await getCategories(url);
-    
-    for (let i = 0; i < data_cat.results.length; i++) { 
-            var option = document.createElement('option');
-            option.innerHTML = data_cat.results[i].name;
-            option.value = data_cat.results[i].name;
-            comboList.appendChild(option);  
-        }
-    document.body.appendChild(comboList);
-  }
-}
-
 async function renderCategories1() {
   let morePagesAvailable = true;
   let beginPage = 1
-  var comboList = document.createElement('datalist');
-  comboList.id = "category_list";
+  let categories = []
+  //fetch api
   while(morePagesAvailable) { 
     if (beginPage == 1)
         {url = `${GENRES_API_URL}?page=${beginPage}`;
         beginPage++;}
     let data_cat = await getCategories(url);
-    
-    for (let i = 0; i < data_cat.results.length; i++) { 
-            var option = document.createElement('option');
-            option.innerHTML = data_cat.results[i].name;
-            option.value = data_cat.results[i].name;
-            comboList.appendChild(option);  
-        }
-    document.body.appendChild(comboList);
+    //console.log(data_cat.results)
+    categories.push(...data_cat.results);
+    console.log(categories)
     if (data_cat.next == null)
         {morePagesAvailable = false;}
     else
         {url = data_cat.next;}
-  }
-
+  }  
+  //Fill combobox
+  var comboList = document.createElement('datalist');
+  comboList.id = "category_list";  
+  for (let i = 0; i < categories.length; i++) { 
+          var option = document.createElement('option');
+          option.innerHTML = categories[i].name;
+          option.value = categories[i].name;
+          comboList.appendChild(option);  
+      }
+  document.body.appendChild(comboList);
+    
 }
 
 renderCategories1();
